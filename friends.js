de5442911c95ebd84c9460c0103d8b80ab8a6024
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-10-15 17:58:43 -0400

    all packages needed added as well as created friends object

---
+++ friends.js
@@ -0,0 +1,28 @@
+var friends = [
+
+    {
+        name: 'Fred Sanford',
+        photo:  "http://www.espn.com/media/pg2/2005/0314/photo/wb/m_sanford_vt.jpg",
+        scores: [3,1,5,5,3,2,3,4,4,1]
+    },
+
+
+    {
+        name: 'Viola Davis',
+        photo: 'https://media.vanityfair.com/photos/5b96760f82aada4712db12d5/16:9/w_2560%2Cc_limit/Viola_Davis_1247_A.jpg',
+        scores: [4,2,3,3,3,4,5,1,2,5]
+
+
+    },
+
+    {
+        name: 'Freddie Highmore',
+        photo: 'https://m.media-amazon.com/images/M/MV5BMjEzNjAzMTgzMV5BMl5BanBnXkFtZTcwNjU2NjA2NQ@@._V1_.jpg',
+        scores: [3,1,1,1,4,3,4,4,4,2]
+
+    }
+];
+
+
+
+module.exports = friends;
