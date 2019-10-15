fef961064e25718156153759a48b67ed626b6e25
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-10-15 18:27:45 -0400

    in the process of refactoring my functions to arrow funcs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,17 +3,24 @@ var friends = require("../data/friends.js")
 module.exports = function(app){
 
     //api GET req.
-    app.get('/api/friends', function(req,res){
+    app.get('/api/friends',(req,res) => {
         res.json(friends);
     })
     
-    app.post('/api/friends', function(req,res){
+    app.post('/api/friends', (req,res)=>{
+        
         
         var diff =0;
         var friendMatch = {
             name:"",
             photo: "",
-            
+            difference: 10000
         }
+
+        let submitdata = req.body;
+        let usrName = userData.name;
+        let usrScore = req.body.scores;
+
+        for(var i=0; i<friend)
     })
 }
