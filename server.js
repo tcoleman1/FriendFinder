b1033c350ac830a28ea4f7bf624c30f823969f45
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-10-15 17:40:48 -0400

    survey and home page completed

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/")
+app.get("/survey", function(req,res){
+    res.render("survey.html")
+})
