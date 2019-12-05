9611cf15dd34a5df74d5c1053f66de47ba723339
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-05 18:57:00 -0500

    removing duplicated const path from file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,15 @@
 var path = require('path');
 
-const path = require('path')
+//const path = require('path')
 
 module.exports = function htmlRoutes(app){
 
     app.get("/", function(req,res) {
-        res.sendfile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function(req,res) {
-        res.sendfile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // If no matching route is found default to home
