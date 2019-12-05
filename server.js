a1b2dce23bccd60fb149ca419167a4a1cacf0b28
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-12-04 19:23:38 -0500

    updated home page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-const path = require("path");
+//const path = require("path");
 
 // Sets up the Express App
 // =============================================================
